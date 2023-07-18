69e1a8018cacc2ff534070c1e34fac08d926e130
abhyrrr <104734000+abhyrrr@users.noreply.github.com>
2023-07-18 13:51:17 -0700

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -3,7 +3,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-//abhir test
+//abhir
 @TeleOp
 public class Teleop extends LinearOpMode {
 
