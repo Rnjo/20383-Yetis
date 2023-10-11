703d7e6661c44abf56c0768c6e39be63a7679189
Rnjo <kotamraju.mihir@gmail.com>
2023-10-10 18:47:12 -0700

    Sdk 9.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetection.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -49,7 +49,7 @@ import java.util.List;
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
 //@Disabled
-public class ConceptTensorFlowObjectDetection extends LinearOpMode {
+public class TensorFlowObjectDetection extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
 
