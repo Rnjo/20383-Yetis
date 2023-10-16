f79378ff6dd7145ee7cbfb79a2a738d56f14037c
Rnjo <kotamraju.mihir@gmail.com>
2023-10-15 17:50:33 -0700

    OpenCv pipelinedone*fingers crossed*

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telep.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.openftc.easyopencv.PipelineRecordingParameters;
 
 //abhir
 @TeleOp
