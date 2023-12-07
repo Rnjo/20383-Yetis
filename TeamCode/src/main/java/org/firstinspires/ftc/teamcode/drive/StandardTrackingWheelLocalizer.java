8ef24c7f5eafff6575f519f52dd16886b81b44dc
Rnjo <kotamraju.mihir@gmail.com>
2023-12-07 15:11:29 -0800

    drive constants *fixed*

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,7 +31,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.6889764; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = -11.9550189141797; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 11.9550189141797; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 6.1; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1.0434489683;
