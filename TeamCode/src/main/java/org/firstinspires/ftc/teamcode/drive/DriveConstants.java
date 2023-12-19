968be718e885cc0faa7ddcdd896a40c68fe224a8
abhyrrr <abhyrrr@gmail.com>
2023-12-18 21:49:39 -0800

    ManualFeedforwardTuning Done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.032 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.02;
-    public static double kStatic = 0.01;
+    public static double kV = 0.022 / rpmToVelocity(MAX_RPM); //Previous value ws 0.33
+    public static double kA = 0.002; //Previous value was 0
+    public static double kStatic = 0.007; //Previous value was 0
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -99,7 +99,7 @@ public class DriveConstants {
      * https://github.com/acmerobotics/road-runner-quickstart/issues/91
 
      */
-    public static double MAX_VEL = 90;
+    public static double MAX_VEL = 34; //Previous value was 90
     public static double MAX_ACCEL = 90;
     public static double MAX_ANG_VEL = Math.toRadians(360);
     public static double MAX_ANG_ACCEL = Math.toRadians(360);
