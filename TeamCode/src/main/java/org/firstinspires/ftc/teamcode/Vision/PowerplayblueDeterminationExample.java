383d24b56f49cbead5b80f6993d0d3cda5c36012
Rnjo <kotamraju.mihir@gmail.com>
2024-03-11 17:08:30 -0700

    Top pos for pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/PowerplayblueDeterminationExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/PowerplayblueDeterminationExample.java
@@ -107,7 +107,8 @@ public class PowerplayblueDeterminationExample extends LinearOpMode
         {
             LEFT,
             CENTER,
-            RIGHT
+            RIGHT,
+            TOP
         }
 
         /*
@@ -123,8 +124,11 @@ public class PowerplayblueDeterminationExample extends LinearOpMode
         static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(0,0);
         static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(415,0);
         static final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(850,0);
+        static final Point REGION4_TOPLEFT_ANCHOR_POINT  =new Point(0, 720);
         static final int REGION_WIDTH = 424;
-        static final int REGION_HEIGHT = 720;
+        static final int REGION_HEIGHT = 620;
+        static final int REGION_WIDTH_TOP = 424;
+        static final int REGION_HEIGHT_TOP = 720;
 
         /*
          * Points which actually define the sample region rectangles, derived from above values
@@ -161,14 +165,20 @@ public class PowerplayblueDeterminationExample extends LinearOpMode
         Point region3_pointB = new Point(
                 REGION3_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
                 REGION3_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+        Point region4_pointA = new Point(
+                REGION4_TOPLEFT_ANCHOR_POINT.x,
+                REGION4_TOPLEFT_ANCHOR_POINT.y);
+        Point region4_pointB = new Point(
+                REGION4_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH_TOP,
+                REGION4_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT_TOP);
 
         /*
          * Working variables
          */
-        Mat region1_Cb, region2_Cb, region3_Cb;
+        Mat region1_Cb, region2_Cb, region3_Cb, region4_Cb;
         Mat YCrCb = new Mat();
         Mat Cb = new Mat();
-        int avg1, avg2, avg3;
+        int avg1, avg2, avg3, avg4;
 
         // Volatile since accessed by OpMode thread w/o synchronization
         private volatile SkystonePosition position = SkystonePosition.LEFT;
@@ -205,6 +215,7 @@ public class PowerplayblueDeterminationExample extends LinearOpMode
             region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
             region2_Cb = Cb.submat(new Rect(region2_pointA, region2_pointB));
             region3_Cb = Cb.submat(new Rect(region3_pointA, region3_pointB));
+            region4_Cb = Cb.submat(new Rect(region4_pointA, region4_pointB));
         }
 
         @Override
@@ -260,6 +271,7 @@ public class PowerplayblueDeterminationExample extends LinearOpMode
             avg1 = (int) Core.mean(region1_Cb).val[0];
             avg2 = (int) Core.mean(region2_Cb).val[0];
             avg3 = (int) Core.mean(region3_Cb).val[0];
+            avg4 = (int) Core.mean(region4_Cb).val[0];
 
             /*
              * Draw a rectangle showing sample region 1 on the screen.
@@ -294,12 +306,19 @@ public class PowerplayblueDeterminationExample extends LinearOpMode
                     BLUE, // The color the rectangle is drawn in
                     2); // Thickness of the rectangle lines
 
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    region4_pointA, // First point which defines the rectangle
+                    region4_pointB, // Second point which defines the rectangle
+                    BLUE, // The color the rectangle is drawn in
+                    2); // Thickness of the rectangle lines
 
             /*
              * Find the max of the 3 averages
              */
             int maxOneTwo = Math.max(avg1, avg2);
-            int max = Math.max(maxOneTwo, avg3);
+            int maxOneThree = Math.max(maxOneTwo, avg3);
+            int max = Math.max(maxOneThree, avg4);
 
             /*
              * Now that we found the max, we actually need to go and
@@ -347,6 +366,21 @@ public class PowerplayblueDeterminationExample extends LinearOpMode
                         input, // Buffer to draw on
                         region3_pointA, // First point which defines the rectangle
                         region3_pointB, // Second point which defines the rectangle
+                        GREEN, // The color the rectangle is drawn in
+                        -1); // Negative thickness means solid fill
+            }
+            else if(max == avg4) // Was it from region 3?
+            {
+                position = SkystonePosition.TOP; // Record our analysis
+
+                /*
+                 * Draw a solid rectangle on top of the chosen region.
+                 * Simply a visual aid. Serves no functional purpose.
+                 */
+                Imgproc.rectangle(
+                        input, // Buffer to draw on
+                        region4_pointA, // First point which defines the rectangle
+                        region4_pointB, // Second point which defines the rectangle
                         GREEN, // The color the rectangle is drawn in
                         -1); // Negative thickness means solid fill
             }
